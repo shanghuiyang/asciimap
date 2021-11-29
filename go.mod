fd428834f4b361fe5ff1ed4ef436600ad9f24dc8
Shanghui Yang <shanghui.yang@grabtaxi.com>
2021-11-29 23:13:28 +0800

    use google s2

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.15
 require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551
+	github.com/paulmach/go.geojson v1.4.0
 	github.com/shanghuiyang/astar v0.0.0-20210817164024-507fb2d0e0a2
-	github.com/spatial-go/geoos v1.0.2
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	"io/ioutil"
 	"os"
 
-	"github.com/shanghuiyang/astar"
+	"github.com/golang/geo/s2"
+	geojson "github.com/paulmach/go.geojson"
 	"github.com/shanghuiyang/astar/tilemap"
-	"github.com/spatial-go/geoos/geojson"
-	"github.com/spatial-go/geoos/planar"
-	"github.com/spatial-go/geoos/space"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -22,11 +20,6 @@ var (
 	mapbbox  *bbox
 	gridSize = 0.00001
 )
-
-type point struct {
-	Lat float64 `json:"lat"`
-	Lon float64 `json:"lon"`
-}
 
 type bbox struct {
 	Left   float64 `json:"left"`
@@ -50,44 +43,38 @@ func main() {
 		os.Exit(1)
 	}
 
-	strategy := planar.NormalStrategy()
 	fc, err := geojson.UnmarshalFeatureCollection([]byte(rawJSON))
 	if err != nil {
 		fmt.Printf("ERROR! %v\n", err)
 		os.Exit(1)
 	}
-	var bb space.Geometry
-	var walls []space.Geometry
+
+	var bb *geojson.Geometry
+	var walls []*s2.Loop
 	for _, f := range fc.Features {
-		if v, ok := f.Properties["isbbox"]; ok && v.(bool) {
-			bb = f.Geometry.Coordinates
+		if !f.Geometry.IsPolygon() {
+			fmt.Printf("WARNING! skip a geometry who isn't polygon\n")
 			continue
 		}
-		walls = append(walls, f.Geometry.Coordinates)
+		if isbbox, _ := f.PropertyBool("isbbox"); isbbox {
+			bb = f.Geometry
+			continue
+		}
+		w := toLoop(f.Geometry)
+		walls = append(walls, w)
 	}
 
-	b := bb.Bound()
-	mapbbox = &bbox{
-		Left:   b.Min.Lon(),
-		Right:  b.Max.Lon(),
-		Top:    b.Max.Lat(),
-		Bottom: b.Min.Lat(),
-	}
-
+	mapbbox = bbound(bb)
 	row := int((mapbbox.Top-mapbbox.Bottom)/gridSize + 0.5)
 	col := int((mapbbox.Right-mapbbox.Left)/gridSize + 0.5)
+	fmt.Println(row, col)
 	m := tilemap.New(row, col)
 	for r := 0; r < row; r++ {
 		for c := 0; c < col; c++ {
-			pt := xy2geo(&astar.Point{X: r, Y: c})
-			p := &space.Point{pt.Lon, pt.Lat}
+			lat, lon := xy2latlon(r, c)
+			pt := s2.PointFromLatLng(s2.LatLngFromDegrees(lat, lon))
 			for _, w := range walls {
-				yes, err := strategy.Intersects(p, w)
-				if err != nil {
-					fmt.Printf("ERROR! %v\n", err)
-					os.Exit(1)
-				}
-				if yes {
+				if w.ContainsPoint(pt) {
 					m.SetWall(r, c)
 				}
 			}
@@ -97,20 +84,17 @@ func main() {
 	mapstr := m.String()
 	ioutil.WriteFile(*mapFile, []byte(mapstr), os.ModePerm)
 	fmt.Printf("map bbox:\n")
-	fmt.Printf("-left:\t\t%11.6f\n", mapbbox.Left)
-	fmt.Printf("-right:\t\t%11.6f\n", mapbbox.Right)
-	fmt.Printf("-top:\t\t%11.6f\n", mapbbox.Top)
-	fmt.Printf("-bottom:\t%11.6f\n", mapbbox.Bottom)
-	fmt.Printf("grid size: \t%11.6f\n", gridSize)
+	fmt.Printf(" left  : %11.6f\n", mapbbox.Left)
+	fmt.Printf(" right : %11.6f\n", mapbbox.Right)
+	fmt.Printf(" top   : %11.6f\n", mapbbox.Top)
+	fmt.Printf(" bottom: %11.6f\n", mapbbox.Bottom)
+	fmt.Printf("grid size: %.6f\n", gridSize)
 	fmt.Printf("%v", mapstr)
 	os.Exit(0)
 }
 
-func xy2geo(p *astar.Point) *point {
-	return &point{
-		Lat: mapbbox.Top - float64(p.X)*gridSize,
-		Lon: mapbbox.Left + float64(p.Y)*gridSize,
-	}
+func xy2latlon(x, y int) (lat, lon float64) {
+	return mapbbox.Top - float64(x)*gridSize, mapbbox.Left + float64(y)*gridSize
 }
 
 func loadMap(file string) ([]byte, error) {
@@ -120,4 +104,45 @@ func loadMap(file string) ([]byte, error) {
 
 	}
 	return rawJSON, nil
+}
+
+func bbound(g *geojson.Geometry) *bbox {
+	b := &bbox{
+		Left:   181,
+		Right:  -181,
+		Top:    -91,
+		Bottom: 91,
+	}
+	for _, poly := range g.Polygon {
+		for _, pt := range poly {
+			if pt[1] < b.Bottom {
+				b.Bottom = pt[1]
+			}
+			if pt[1] > b.Top {
+				b.Top = pt[1]
+			}
+			if pt[0] < b.Left {
+				b.Left = pt[0]
+			}
+			if pt[0] > b.Right {
+				b.Right = pt[0]
+			}
+		}
+	}
+	return b
+}
+
+func toLoop(g *geojson.Geometry) *s2.Loop {
+	if !g.IsPolygon() {
+		return nil
+	}
+	if len(g.Polygon) == 0 {
+		return nil
+	}
+	var pts []s2.Point
+	for i := 0; i < len(g.Polygon[0])-1; i++ {
+		pt := g.Polygon[0][i]
+		pts = append(pts, s2.PointFromLatLng(s2.LatLngFromDegrees(pt[1], pt[0])))
+	}
+	return s2.LoopFromPoints(pts)
 }
